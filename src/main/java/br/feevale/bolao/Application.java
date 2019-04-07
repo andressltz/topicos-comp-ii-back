ebf52c7cda9d6d8b18312b84ce89cf0f3eba3d03
andressltz <andreswinck@gmail.com>
2019-04-07 14:53:49 -0300

    add configuration to Google App Engine

--- src/main/java/br/feevale/bolao/Application.java
+++ src/main/java/br/feevale/bolao/Application.java
@@ -2,6 +2,7 @@ package br.feevale.bolao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class Application {
@@ -9,6 +10,12 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         System.out.println("Running... To update the database run http://localhost:8181/classification/update");
+    }
+
+    @RequestMapping("/_ah/health")
+    public String healthy() {
+        // Message body required though ignored
+        return "Still surviving.";
     }
 
 }
