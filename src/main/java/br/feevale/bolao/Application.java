1b01b992d6192f7519899b0b0d5e07597da0e445
andressltz <andreswinck@gmail.com>
2019-04-07 15:21:38 -0300

    add configuration to Google App Engine

--- src/main/java/br/feevale/bolao/Application.java
+++ src/main/java/br/feevale/bolao/Application.java
@@ -2,6 +2,11 @@ package br.feevale.bolao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
@@ -9,13 +14,21 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        System.out.println("Running... To update the database run http://localhost:8181/classification/update");
+        System.out.println("Running... To update the database run http://localhost:8080/classification/update");
     }
 
     @RequestMapping("/_ah/health")
     public String healthy() {
         // Message body required though ignored
         return "Still surviving.";
+    }
+
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory() {
+        JettyServletWebServerFactory factory = new JettyServletWebServerFactory();
+        factory.setPort(8080);
+        factory.setContextPath("");
+        return factory;
     }
 
 }
