445f5bc271ad918c6f613733c4c1fbd08156c9fb
andressltz <andreswinck@gmail.com>
2019-04-14 23:41:55 -0300

    spring boot

--- src/main/java/br/feevale/bolao/Application.java
+++ src/main/java/br/feevale/bolao/Application.java
@@ -2,19 +2,21 @@ package br.feevale.bolao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
-import org.springframework.boot.web.server.ErrorPage;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         System.out.println("Running... To update the database run http://localhost:8080/classification/update");
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
     }
 
     @RequestMapping("/_ah/health")
@@ -23,12 +25,12 @@ public class Application {
         return "Still surviving.";
     }
 
-    @Bean
-    public ConfigurableServletWebServerFactory webServerFactory() {
-        JettyServletWebServerFactory factory = new JettyServletWebServerFactory();
-        factory.setPort(8080);
-        factory.setContextPath("");
-        return factory;
-    }
+//    @Bean
+//    public ConfigurableServletWebServerFactory webServerFactory() {
+//        JettyServletWebServerFactory factory = new JettyServletWebServerFactory();
+//        factory.setPort(8080);
+//        factory.setContextPath("");
+//        return factory;
+//    }
 
 }
